5f1cf9258c3456c4656d19210002e1b87abcaac2
Suriadi Vajrakaruna <suriadivajrakaruna432@gmail.com>
2020-04-14 17:35:04 +0700

    AnalgoKu7

--- AnalgoKu6/dfs.cpp
+++ AnalgoKu6/dfs.cpp
@@ -13,20 +13,16 @@ void print(int data) { std::cout << data << ' '; }
 
 int main(int argc, char const **argv)
 {
-    const size_t graph_size = 8;
+    const size_t graph_size = 5;
     Analgo::Graph<int> g(graph_size);
 
     g.add_edge(1, 2);
-    g.add_edge(1, 3);
+    g.add_edge(1, 4);
     g.add_edge(2, 3);
-    g.add_edge(2, 4);
     g.add_edge(2, 5);
     g.add_edge(3, 5);
-    g.add_edge(3, 7);
-    g.add_edge(3, 8);
+    g.add_edge(3, 4);
     g.add_edge(4, 5);
-    g.add_edge(5, 6);
-    g.add_edge(7, 8);
 
     try
     {
